b7dde23cff4412d377a87044c9dab1462e6f9c4b
Subham Sarkar <122025008+subham631@users.noreply.github.com>
2024-01-09 05:21:50 +0000

    data type and conversions

---
+++ 02_datatype.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer version
+//code readability is the first preference
+//alert(1+1) // We are using nodejs, not browser
+
+let name = "Subham";
+let age = 20;
+let isGraduate = false;
+let temp = null;
+//undefined => when you don't assign the value
+//symbol => unique (more use in react)
+
+// object
+console.log(typeof name); 
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object

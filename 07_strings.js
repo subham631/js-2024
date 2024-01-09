e2f31ebf5c7158abc5a135248e8703b7ddbbb947
Subham Sarkar <122025008+subham631@users.noreply.github.com>
2024-01-09 13:59:36 +0000

    strings

---
+++ 07_strings.js
@@ -0,0 +1,14 @@
+const name = "subham";
+const age = 20;
+
+console.log(name + age + 5);  // not recommended
+
+console.log(`My name is ${name} and my age is ${age}`);
+
+const subject = new String("Geography");
+
+console.log(subject.toLowerCase());
+console.log(subject.length);
+
+
+// go to the console in browser and check all the functions of strings

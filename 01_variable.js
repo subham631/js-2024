eb4f8264f1f3b25c943c315b5b8c226e914a8b2f
Subham Sarkar <122025008+subham631@users.noreply.github.com>
2024-03-08 17:40:36 +0000

    some change

--- 01_variable.js
+++ 01_variable.js
@@ -2,7 +2,7 @@ console.log("Hello World");
 const accountId = "sarkar124";
 let password = "2703@ct";
 var email = "sarkarsubham087@email.com"; //not prefer to use 'var' because of issue in block scope and functional scope
-city = "Kolkata"; // not recommended
+city = "Kolkata"; // not recommended without let
 let accountState; //if not defined then it shows undefined
 
 console.table([accountId, email, password]);
